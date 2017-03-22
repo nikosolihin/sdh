41855c2e68f89d78c03f00dff54c1571107f5a03
Niko Solihin <pipeandpilcrow@gmail.com>
2017-03-23 01:57:48 +0700

    deploy prep

--- src/php/front-page.php
+++ src/php/front-page.php
@@ -9,7 +9,11 @@ $context['hero'] = get_field('hero', 'option');
 $context['placeholder'] = get_field('hero_placeholder', 'option');
 
 // Blocks - Only mobile
-$context['home_blocks'] = get_field('home_blocks', 'option');
+$context['home_blocks']['title'] = get_field('home_blocks_title', 'option');
+$context['home_blocks']['blocks'] = get_field('home_blocks', 'option');
+
+// Mobile Hero
+$context['mobile_hero'] = get_field('mobile_hero', 'option');
 
 // News
 $context['news'] = array(
